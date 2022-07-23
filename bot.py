6c3986b69e7273f9df43dad0469ae92ff54e2a60
Dante <48799984+dantefleschpereira@users.noreply.github.com>
2022-07-23 12:17:43 -0300

    bot update

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 import time
 
+from selenium.webdriver.common.by import By
+
+
 driver = webdriver.Chrome()
 driver.get('https://covid.saude.gov.br/')
-elemento = driver.find_element_by_xpath('/html/body/app-root/ion-app/ion-router-outlet/app-home/ion-content/div[1]/div[2]/ion-button')
-time.sleep(7)  
-elemento.click()
-
+element = driver.find_element(By.XPATH, '/html/body/app-root/ion-app/ion-router-outlet/app-home/ion-content/div[1]/div[2]/ion-button')
+time.sleep(7)
+element.click()
